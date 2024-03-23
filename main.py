8b19cca34086629e95038193819c301159d95253
hmull1128 <hmull1128@gmail.com>
2024-03-23 09:54:27 -0400

    a

--- main.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask, render_template, url_for, redirect, request, flash
-import os
-
-@main.route('/', methods=['POST', 'GET'])
-def index():
-    if request.method == 'GET':
-        return render_template('index.html')
