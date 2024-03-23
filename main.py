2adf2c3c7f6be3c08f31cb571ec439a0851efb6c
hmull1128 <141769135+hmull1128@users.noreply.github.com>
2024-03-23 09:32:46 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template
 
-@main.route('/')
+@main.route('/', methods=['POST', 'GET'])
 def index():
-    return render_template('index.html')
+    if request.method == 'GET':
+        return render_template('index.html')
