1cdb1a035be8b6d769c070551a82b6cd3d80e17f
noahstanford <nstanford20061@gmail.com>
2024-03-23 14:23:44 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 import openai
 from openai import OpenAI
-client = OpenAI(api_key='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')
+client = OpenAI(api_key='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')
 
 app = create_app()
 
@@ -23,14 +23,19 @@ def index():
         elif request.method == 'POST':
             factors = ["1","1","1","1","1","1","1"]
             nums = getNums()
-            paragraph = "Give me a list of songs that meet the criteria on a scale of 0-5. Happy(" + nums[0] + "), Sad(" + nums[1] + "), calm(" + nums[2] + "), energetic(" + nums[3] + "), nostalgic(" + nums[4] + "), romantic(" + nums[5] + "), and inspirational(" + nums[6] + "). Return one string of 5 song titles separated by a dash without the artist"
-            #message = {"role": "user", "content": paragraph}
-            #completion = client.chat.completions.create(
-                #model="gpt-3.5-turbo",
-                #messages = [{"role": "user", "content": paragraph}]
-            #)
-            #print(completion.choices[0].message)
+            paragraph = "Give me a list of songs that meet the criteria on a scale of 0-5. Happy(" + nums[0] + "), Sad(" + nums[1] + "), calm(" + nums[2] + "), energetic(" + nums[3] + "), nostalgic(" + nums[4] + "), romantic(" + nums[5] + "), and inspirational(" + nums[6] + "). Return one string of 5 song titles separated by a semicolon without the artist"
+            message = {"role": "user", "content": paragraph}
+            completion = client.chat.completions.create(
+                model="gpt-3.5-turbo",
+                messages = [{"role": "user", "content": paragraph}]
+            )
+            item = completion.choices[0].message.content
             print(paragraph)
+            print(completion.choices[0].message)
+            song_names_str = item.strip('"')  # Remove leading and trailing quotes
+            song_names_list = [song.strip() for song in song_names_str.split(';')]
+
+            print(song_names_list)
             return render_template('index.html')
 
 if __name__ == '__main__':
