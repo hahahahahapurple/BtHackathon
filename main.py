861d0f7374c219a10120cd7e508217a122cb6a6a
hmull1128 <141769135+hmull1128@users.noreply.github.com>
2024-03-23 09:27:46 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'supersecretkey'
-
-@app.route('/')
-def hello_world():
-  return render_template('login.html')
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug='True')
+@main.route('/')
+def index():
+    return render_template('index.html')
