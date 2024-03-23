8299c3d1a6e638a20a5313671705ef4f2ceacf07
hmull1128 <141769135+hmull1128@users.noreply.github.com>
2024-03-23 14:03:31 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,15 +5,15 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 import openai
 from openai import OpenAI
-client = OpenAI(api_key='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')
+client = OpenAI(api_key='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')
 
 app = create_app()
 
 def getNums():
     nums = []
     for i in range(7):
-        item = request.form.get("SOMETHING" + str(i + 1))
-        nums.append(item)
+        item = request.form.get("mood_input_" + str(i + 1))
+        nums.append(str(item))
     return nums
 
 @app.route('/', methods=['POST', 'GET'])
@@ -22,16 +22,15 @@ def index():
             return render_template('index.html')
         elif request.method == 'POST':
             factors = ["1","1","1","1","1","1","1"]
-            paragraph = "I am going to give you a list of critera for songs. Here are the criteria:"
             nums = getNums()
-            for i in range(7):
-                paragraph += (" " + factors[i] + ": " + str(nums[i]) + "\n")
-            message = {"role": "user", "content": paragraph}
-            completion = client.chat.completions.create(
-                model="gpt-3.5-turbo",
-                messages = [{"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}]
-            )
-            print(completion.choices[0].message)
+            paragraph = "Give me a list of songs that meet the criteria on a scale of 0-5. Happy(" + nums[0] + "), Sad(" + nums[1] + "), calm(" + nums[2] + "), energetic(" + nums[3] + "), nostalgic(" + nums[4] + "), romantic(" + nums[5] + "), and inspirational(" + nums[6] + "). Return one string of 5 song titles separated by a dash without the artist"
+            #message = {"role": "user", "content": paragraph}
+            #completion = client.chat.completions.create(
+                #model="gpt-3.5-turbo",
+                #messages = [{"role": "user", "content": paragraph}]
+            #)
+            #print(completion.choices[0].message)
+            print(paragraph)
             return render_template('index.html')
 
 if __name__ == '__main__':
