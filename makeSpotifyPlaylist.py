a27bde6be1aa16309ca6bfb901a1f98c5cf4cd40
hahahahahapurple <157333917+hahahahahapurple@users.noreply.github.com>
2024-03-23 09:49:50 -0400

    Update makeSpotifyPlaylist.py

--- makeSpotifyPlaylist.py
+++ makeSpotifyPlaylist.py
@@ -14,3 +14,18 @@ def getUserHistory(sp, user):
             trackURI.append(j['track']['uri'])
             tracks.append(j['track']['name']+" by "+ j['track']['artists'][0]['name'])
     return trackURI, tracks
+
+def getAudio(sp, trackURI):
+    feature = []
+    featureTotal = []
+    m = 100
+    for i in range(0, len(trackURI), m):
+        audio = sp.audio_features(trackURI[i:i+max])
+        time.sleep(1)
+        for j in range(len(audio)):
+            feature.append(audio[j]['danceability'])
+            feature.append(audio[j]['energy'])
+            feature.append(audio[j]['valence'])
+            featureTotal.append(feature)
+        feature = []
+    return featureTotal
