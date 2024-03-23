5341b3997df54acf86604356a1b41ab6a16e6e47
hmull1128 <141769135+hmull1128@users.noreply.github.com>
2024-03-23 09:30:25 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, url_for, redirect, flash
+from project import main
+import os
+
+app = Flask(__name__)
+
+app = create_app()
+app.secret_key = os.urandom(12)
+date = '2022/1/19'
+oauth = OAuth(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+app.run(host='0.0.0.0', port=81,debug = True)
