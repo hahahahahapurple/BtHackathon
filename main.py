e119f4d37a11ee407492e3cdbbc7478f57cca25a
hmull1128 <141769135+hmull1128@users.noreply.github.com>
2024-03-23 09:36:56 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template
+from flask import Flask, render_template, url_for, redirect, request, flash
+import os
 
 @main.route('/', methods=['POST', 'GET'])
 def index():
